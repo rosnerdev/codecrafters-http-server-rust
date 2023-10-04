1b427ba0945c7cf65294953125b3768c9eae4e94
rosnerdev <83503879+rosnerdev@users.noreply.github.com>
2023-10-04 16:38:25 +0300

    pass 3rd task

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use std::net::TcpListener;
-use std::io::Write;
+use std::io::{BufReader, BufRead, Write};
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:4221").unwrap();
@@ -7,11 +7,28 @@ fn main() {
     for stream in listener.incoming() {
         match stream {
             Ok(mut stream) => {
-                stream
-                    .write_all("HTTP/1.1 200 OK\r\n\r\n".as_bytes())
-                    .unwrap();
-                stream.flush().unwrap();
+                let mut reader = BufReader::new(stream.try_clone().unwrap());
+                let mut req_str = String::new();
+
+                reader.read_line(&mut req_str).unwrap();
                 
+                let _path = req_str.split(" ").nth(1);
+                match _path {
+                    Some(path) => {
+                        if path == "/" {
+                            stream
+                                .write_all("HTTP/1.1 200 OK\r\n\r\n".as_bytes())
+                                .unwrap();
+                            stream.flush().unwrap();
+                        } else {
+                            stream
+                                .write_all("HTTP/1.1 404 Not Found\r\n\r\n".as_bytes())
+                                .unwrap();
+                            stream.flush().unwrap();
+                        }
+                    }
+                    None => {}
+                }
             }
             Err(e) => {
                 println!("error: {}", e);
