4dbcd718d9ac736358e7ca9c245bfc222ea19173
rosnerdev <83503879+rosnerdev@users.noreply.github.com>
2023-10-04 15:49:41 +0300

    pass 2st stage

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,13 @@
 use std::net::TcpListener;
+use std::io::prelude::*;
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:4221").unwrap();
     
     for stream in listener.incoming() {
         match stream {
-            Ok(_stream) => {
-                println!("accepted new connection");
+            Ok(mut stream) => {
+                stream.write("HTTP/1.1 200 OK\r\n\r\n".as_bytes()).unwrap();
             }
             Err(e) => {
                 println!("error: {}", e);
