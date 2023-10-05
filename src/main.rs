b262830c378fe2fa469b60820f9fa7717bf8abda
rosnerdev <83503879+rosnerdev@users.noreply.github.com>
2023-10-06 00:15:41 +0300

    commit...

--- src/main.rs
+++ src/main.rs
@@ -81,7 +81,9 @@ fn main() {
                             let mut file = File::create(file_path).unwrap();
                             let file_body = req_str.split("\r\n\r\n").last().unwrap();
 
-                            let file_body = file_body.replace("\r\n", "\n");
+                            let file_body = file_body.replace("\0", "");
+                            let file_body = file_body.replace(r"\r\n", "\n");
+                            let file_body = file_body.replace(r"\n", "\n");
                             file.write_all(&file_body.as_bytes()).unwrap();
 
                             stream
